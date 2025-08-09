92294839c6dad94c91f26a6ac6d639ac6732b4a8
gabriel-assana <gabrielhhz2@gmail.com>
2025-08-09 19:27:04 -0300

    feat: criando context da sidebar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,31 @@
-import type { AppProps } from "next/app";
+import { AppProps } from 'next/app';
+import { ChakraProvider } from '@chakra-ui/react';
+import { QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+import { SidebarDrawerProvider } from '../contexts/SidebarDrawerContext';
+import { queryClient } from '../services/queryClient';
+import { makeServer } from '../services/mirage';
+import { theme } from '../styles/theme';
+
+// const isDev = process.env.NODE_ENV === 'development';
+
+// if (isDev) {
+makeServer();
+// }
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={theme}>
+        <SidebarDrawerProvider>
+          <Component {...pageProps} />
+        </SidebarDrawerProvider>
+      </ChakraProvider>
+
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
 }
+
+export default MyApp;
